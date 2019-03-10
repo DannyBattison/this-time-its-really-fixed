239befbc16f7209019c02fe13a56e941eb591bf4
Danny Battison <captain@danny.gg>
2019-03-10 11:52:58 +0000

    Add search term entity

---
+++ src/Entity/SearchTerm.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SearchTermRepository")
+ */
+class SearchTerm
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $query;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $lastSynced;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuery(): ?string
+    {
+        return $this->query;
+    }
+
+    public function setQuery(string $query): self
+    {
+        $this->query = $query;
+
+        return $this;
+    }
+
+    public function getLastSynced(): ?\DateTimeInterface
+    {
+        return $this->lastSynced;
+    }
+
+    public function setLastSynced(\DateTimeInterface $lastSynced): self
+    {
+        $this->lastSynced = $lastSynced;
+
+        return $this;
+    }
+}
